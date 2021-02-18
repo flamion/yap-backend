b1072cd8438ab9ab2a95697415bcc6157819d969
flamion <laberschleichi@gmail.com>
2021-02-18 14:55:50 +0100

    Changes to main for testing

--- src/main/java/dev/dragoncave/yap/backend/Main.java
+++ src/main/java/dev/dragoncave/yap/backend/Main.java
@@ -1,26 +1,15 @@
 package dev.dragoncave.yap.backend;
 
-import com.google.gson.Gson;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-
+import javax.xml.crypto.Data;
 
 public class Main {
 
     private static final String DATABASE_URL = "jdbc:sqlite:database.db";
 
     public static void main(String[] args) {
-        var gson = new Gson();
-        try (var con = DriverManager.getConnection(DATABASE_URL)) {
-
-            PreparedStatement statement = con.prepareStatement("SELECT * FROM user");
-            var resultSet = statement.executeQuery();
-            System.out.println("Size: " + resultSet.getFetchSize());
-
-            while (resultSet.next()) {
-                System.out.println("user_id: " + resultSet.getInt("user_id") + "  username: " + resultSet.getString("username") + "   createDate: " + resultSet.getString("create_date"));
-            }
-
+        try {
+            System.out.println(DatabaseManager.getInstance().getUserJson(1L));
+            System.out.println(DatabaseManager.getInstance().getEntryJson(1L));
         } catch (Exception e) {
             e.printStackTrace();
         }
