e2e69b0a1afbd34edfe38845bee39698f10924d0
flamion <laberschleichi@gmail.com>
2021-03-07 17:11:25 +0100

    Added a test for the new group creation and deletion methods

--- src/test/java/DatabaseManagerJsonTest.java
+++ src/test/java/DatabaseManagerJsonTest.java
@@ -1,4 +1,6 @@
 import dev.dragoncave.yap.backend.DatabaseManager;
+import dev.dragoncave.yap.backend.Group;
+import dev.dragoncave.yap.backend.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +23,27 @@ public class DatabaseManagerJsonTest {
                 "{\"entryID\":1,\"creator\":{\"userid\":1,\"username\":\"testuser\",\"createDate\":1612895671000,\"lastLogin\":1612895671000,\"emailAddress\":\"test@test.com\"},\"createDate\":1612650686000,\"dueDate\":1612737086000,\"title\":\"Test Title\",\"description\":\"Test Description\"}",
                 DatabaseManager.getInstance().getEntryJson(1L)
         );
+    }
+
+    @Test
+    @DisplayName("Group creation and deletion test")
+    public void testGroupCreation() throws Exception {
+        DatabaseManager instance = DatabaseManager.getInstance();
+        long testUserid = instance.createUser("testuser", "testpassword", System.currentTimeMillis(), System.currentTimeMillis(), "testuser@test.test");
+        long testGroupid = instance.createGroup(
+                "Test Group",
+                instance.getUserByID(testUserid)
+        );
+
+        Group testGroup = instance.getGroupByID(testGroupid);
+        User testUser = instance.getUserByID(testUserid);
+
+        assert testGroup != null;
+        assert testUser != null;
+
+        instance.deleteGroup(testGroupid);
+
+        assert instance.getGroupByID(testGroupid) == null;
+
     }
 }
