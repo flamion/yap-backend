c7413123834846cd1f56f627227a1a79d9eb16c8
flamion <laberschleichi@gmail.com>
2021-03-24 20:19:03 +0100

    Added second method to the Rest hello world now you can go to /greeting/{id} and get choose your greeting id :P

--- src/main/java/dev/dragoncave/yap/backend/rest/GreetingController.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/GreetingController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import dev.dragoncave.yap.backend.rest.objects.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class GreetingController {
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/greeting/{id}")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name, @PathVariable Long id) {
+        return new Greeting(id, String.format(template, name));
     }
 }
