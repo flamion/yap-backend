5ace061a9a635b898428ae655542e4b7b78ebd17
flamion <laberschleichi@gmail.com>
2021-03-24 21:57:11 +0100

    Testing

--- src/main/java/dev/dragoncave/yap/backend/Main.java
+++ src/main/java/dev/dragoncave/yap/backend/Main.java
@@ -10,6 +10,9 @@ public class Main {
             System.out.println(instance.getUserJson(2));
             System.out.println(instance.getGroupJson(1));
             System.out.println(instance.getEntryJson(5));
+            System.out.println(System.getenv("DATABASE_TYPE"));
+
+            instance.createUser("samantharose", "horny", System.currentTimeMillis(), System.currentTimeMillis(), "yes@yes.horny");
         } catch (Exception e) {
             e.printStackTrace();
         }
