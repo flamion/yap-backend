7fd95c9fb8388d599e5f48d088a20f47272bdfb7
flamion <laberschleichi@gmail.com>
2021-03-24 19:43:08 +0100

    Rest API Hello world :)

--- src/main/java/dev/dragoncave/yap/backend/rest/RESTServer.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/RESTServer.java
@@ -1,7 +1,13 @@
 package dev.dragoncave.yap.backend.rest;
 
-public class RESTServer {
-    public static void main(String[] args) {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class RESTServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RESTServer.class, args);
     }
+
 }
