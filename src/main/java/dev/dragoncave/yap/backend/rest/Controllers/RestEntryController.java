6ee6268b0de518700dd382294cd92fc2eaaf4ce8
flamion <laberschleichi@gmail.com>
2021-03-29 15:49:16 +0200

    Refactor: Renamed EntryController to RestEntryController

--- src/main/java/dev/dragoncave/yap/backend/rest/Controllers/RestEntryController.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/Controllers/RestEntryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.SQLException;
 
 @RestController
-public class EntryController {
+public class RestEntryController {
     private static final DatabaseManager dbmanager = DatabaseManager.getInstance();
 
     @GetMapping("/entry/{id}")
