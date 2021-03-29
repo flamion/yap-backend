e7ab6239fc2710632b959b17a95baf218b300479
flamion <laberschleichi@gmail.com>
2021-03-29 16:18:48 +0200

    Removed unused import

--- src/main/java/dev/dragoncave/yap/backend/rest/Controllers/RestUserController.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/Controllers/RestUserController.java
@@ -1,7 +1,6 @@
 package dev.dragoncave.yap.backend.rest.Controllers;
 
 import dev.dragoncave.yap.backend.DatabaseManager;
-import dev.dragoncave.yap.backend.rest.objects.Entry;
 import dev.dragoncave.yap.backend.rest.objects.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
