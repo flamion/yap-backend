a30a9a544e800e0b9431e1bae1aed89f9cde35ef
flamion <laberschleichi@gmail.com>
2021-04-01 03:49:22 +0200

    Move back to sqlite for now

--- src/main/java/dev/dragoncave/yap/backend/databasemanagers/connections/ConnectionController.java
+++ src/main/java/dev/dragoncave/yap/backend/databasemanagers/connections/ConnectionController.java
@@ -6,15 +6,16 @@ import java.sql.SQLException;
 
 public class ConnectionController {
     private final static ConnectionController instance = new ConnectionController();
-    //private static final String DATABASE_URL = "jdbc:sqlite:database.db";
-    private static final String DATABASE_URL = "jdbc:postgresql:testseite";
+    private static final String DATABASE_URL = "jdbc:sqlite:database.db";
+    //private static final String DATABASE_URL = "jdbc:postgresql:testseite";
     private static Connection dbcon;
 
 
     private ConnectionController() {
         try {
             System.out.println(DATABASE_URL);
-            dbcon = DriverManager.getConnection(DATABASE_URL, System.getenv("DB_USERNAME"), System.getenv("DB_PASS"));
+            //dbcon = DriverManager.getConnection(DATABASE_URL, System.getenv("DB_USERNAME"), System.getenv("DB_PASS"));
+            dbcon = DriverManager.getConnection(DATABASE_URL);
         } catch (SQLException e) {
             e.printStackTrace();
         }
