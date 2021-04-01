bed4c06c285b703f074d6f70f7414599c9f360dd
flamion <laberschleichi@gmail.com>
2021-04-01 03:27:44 +0200

    Use postgres

--- src/main/java/dev/dragoncave/yap/backend/databasemanagers/connections/ConnectionController.java
+++ src/main/java/dev/dragoncave/yap/backend/databasemanagers/connections/ConnectionController.java
@@ -6,14 +6,15 @@ import java.sql.SQLException;
 
 public class ConnectionController {
     private final static ConnectionController instance = new ConnectionController();
-    private static final String DATABASE_URL = "jdbc:sqlite:database.db";
+    //private static final String DATABASE_URL = "jdbc:sqlite:database.db";
+    private static final String DATABASE_URL = "jdbc:postgresql:testseite";
     private static Connection dbcon;
 
 
     private ConnectionController() {
         try {
             System.out.println(DATABASE_URL);
-            dbcon = DriverManager.getConnection(DATABASE_URL);
+            dbcon = DriverManager.getConnection(DATABASE_URL, System.getenv("DB_USERNAME"), System.getenv("DB_PASS"));
         } catch (SQLException e) {
             e.printStackTrace();
         }
