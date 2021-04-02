f7df5cf34ab71e696b512641d5ed59f716d40505
flamion <laberschleichi@gmail.com>
2021-04-02 16:48:27 +0200

    Removed deprecated method getInstance() from ConnectionController and removed unused cb connection and constructor code

--- src/main/java/dev/dragoncave/yap/backend/databasemanagers/connections/ConnectionController.java
+++ src/main/java/dev/dragoncave/yap/backend/databasemanagers/connections/ConnectionController.java
@@ -4,14 +4,11 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionController {
-    private final static ConnectionController instance = new ConnectionController();
     //private static final String DATABASE_URL = "jdbc:sqlite:database.db";
     private static final String DATABASE_URL = "jdbc:postgresql:testseite";
-    private static Connection dbcon;
     private static final HikariDataSource dataSource;
 
     static {
@@ -25,20 +22,6 @@ public class ConnectionController {
     }
 
     private ConnectionController() {
-        try {
-            System.out.println(System.getenv("DB_USERNAME") + " " + System.getenv("DB_PASS"));
-            dbcon = DriverManager.getConnection(DATABASE_URL, System.getenv("DB_USERNAME"), System.getenv("DB_PASS"));
-            //dbcon = DriverManager.getConnection(DATABASE_URL);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        assert dbcon != null;
-
-
-    }
-
-    public static ConnectionController getInstance() {
-        return instance;
     }
 
     public static Connection getConnection() throws SQLException {
