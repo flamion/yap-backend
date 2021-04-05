64657e71dba70b1ab5e6200db75f247b8c41acd1
flamion <laberschleichi@gmail.com>
2021-04-05 22:58:34 +0200

    Refactor: Reformat and optimized imports

--- src/main/java/dev/dragoncave/yap/backend/rest/Controllers/security/SecurityController.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/Controllers/security/SecurityController.java
@@ -1,11 +1,11 @@
 package dev.dragoncave.yap.backend.rest.Controllers.security;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/security")
 public class SecurityController {
-
 
 
 }
