f93e646cc85def8913736879cc58b970df4a9aee
flamion <laberschleichi@gmail.com>
2021-04-05 20:05:30 +0200

    Added PasswordUtils class with helper methods to handle passwords

--- src/main/java/dev/dragoncave/yap/backend/Main.java
+++ src/main/java/dev/dragoncave/yap/backend/Main.java
@@ -1,14 +1,46 @@
 package dev.dragoncave.yap.backend;
 
+import dev.dragoncave.yap.backend.databasemanagers.connections.ConnectionController;
+import dev.dragoncave.yap.backend.rest.Controllers.security.PasswordUtils;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Base64;
+
 public class Main {
 
     private static final String DATABASE_URL = "jdbc:sqlite:database.db";
 
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) throws Exception {
+        String test = "ABCDEF";
+        String test2;
+        byte[] bytes = new byte[16];
+        new SecureRandom().nextBytes(bytes);
+        System.out.println(Arrays.toString(bytes));
+        String formatted = new String(bytes, StandardCharsets.UTF_8);
+        System.out.println(formatted);
+        System.out.println(Arrays.toString(formatted.getBytes(StandardCharsets.UTF_8)));
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("--------------------------------------\n");
+
+        System.out.println(Arrays.toString(bytes));
+        String base64 = Base64.getEncoder().encodeToString(bytes);
+        System.out.println(base64);
+        System.out.println(Arrays.toString(Base64.getDecoder().decode(base64)));
+
+        System.out.println("--------------------------------------\n");
+
+        String password = "Super duper secret password";
+
+        String salt = PasswordUtils.getBase64Salt();
+        String hash = PasswordUtils.getHash(password, salt);
+
+        System.out.println(hash);
+
+        System.out.println(PasswordUtils.isExpectedPassword(password, salt, hash));
     }
 }
---
+++ src/main/java/dev/dragoncave/yap/backend/rest/Controllers/security/PasswordUtils.java
@@ -0,0 +1,58 @@
+package dev.dragoncave.yap.backend.rest.Controllers.security;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class PasswordUtils {
+
+    public static String getBase64Salt() {
+        return Base64.getEncoder().encodeToString(getSalt());
+    }
+
+    public static byte[] getSalt() {
+        byte[] bytes = new byte[16];
+        new SecureRandom().nextBytes(bytes);
+        return bytes;
+    }
+
+    public static boolean isExpectedPassword(String password, String base64Salt, String expectedHash) throws NoSuchAlgorithmException {
+        return isExpectedPassword(password, Base64.getDecoder().decode(base64Salt), expectedHash);
+    }
+
+    public static boolean isExpectedPassword(String password, byte[] salt, String expectedHash) throws NoSuchAlgorithmException {
+        return getHash(password, salt).equals(expectedHash);
+    }
+
+    public static String getHash(String input, String base64Salt) throws NoSuchAlgorithmException {
+        return getHash(input, Base64.getDecoder().decode(base64Salt));
+    }
+
+    public static String getHash(String input, byte[] salt) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA3-512");
+        digest.update(input.getBytes(StandardCharsets.UTF_8));
+        digest.update(salt);
+        byte[] encodedHash = digest.digest();
+        return bytesToHex(encodedHash);
+    }
+
+    public static String getHash(String input) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA3-512");
+        byte[] encodedHash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+        return bytesToHex(encodedHash);
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
