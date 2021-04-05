9f15190c9a08b06caaa80c80b10a96e10f4ebf44
flamion <laberschleichi@gmail.com>
2021-04-05 20:15:13 +0200

    Removed unused imports

--- src/main/java/dev/dragoncave/yap/backend/rest/Controllers/security/SecurityController.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/Controllers/security/SecurityController.java
@@ -1,15 +1,7 @@
 package dev.dragoncave.yap.backend.rest.Controllers.security;
 
-import dev.dragoncave.yap.backend.databasemanagers.connections.ConnectionController;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/security")
