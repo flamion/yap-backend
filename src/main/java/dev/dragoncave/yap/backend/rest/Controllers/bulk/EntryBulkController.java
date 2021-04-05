129e8b25cbf1e53c7165538dd058f4c287e9e0e9
flamion <laberschleichi@gmail.com>
2021-04-05 14:28:23 +0200

    Removed unused Imports

--- src/main/java/dev/dragoncave/yap/backend/rest/Controllers/bulk/EntryBulkController.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/Controllers/bulk/EntryBulkController.java
@@ -1,20 +1,11 @@
 package dev.dragoncave.yap.backend.rest.Controllers.bulk;
 
-import dev.dragoncave.yap.backend.databasemanagers.EntryController;
-import dev.dragoncave.yap.backend.databasemanagers.UserController;
-import dev.dragoncave.yap.backend.rest.objects.Entry;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/bulk/entry")
 public class EntryBulkController {
-
 
 
 }
