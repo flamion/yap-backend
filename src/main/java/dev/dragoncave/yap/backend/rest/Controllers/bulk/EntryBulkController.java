7ac098326885c8cd68d694becb4df3a863f7d62c
flamion <laberschleichi@gmail.com>
2021-04-05 14:17:16 +0200

    Add BulkEntryController

---
+++ src/main/java/dev/dragoncave/yap/backend/rest/Controllers/bulk/EntryBulkController.java
@@ -0,0 +1,20 @@
+package dev.dragoncave.yap.backend.rest.Controllers.bulk;
+
+import dev.dragoncave.yap.backend.databasemanagers.EntryController;
+import dev.dragoncave.yap.backend.databasemanagers.UserController;
+import dev.dragoncave.yap.backend.rest.objects.Entry;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/bulk/entry")
+public class EntryBulkController {
+
+
+
+}
