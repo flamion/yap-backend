d8b8d6afb3190e4011da9294f4ddbf19e4cf48cd
flamion <laberschleichi@gmail.com>
2021-04-07 16:47:24 +0200

    Removed password update endpoint

--- src/main/java/dev/dragoncave/yap/backend/rest/security/controllers/SecurityController.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/security/controllers/SecurityController.java
@@ -19,41 +19,4 @@ import java.util.HashMap;
 @RequestMapping("/security")
 public class SecurityController {
 
-    @PostMapping("/password/update")
-    public ResponseEntity<?> changePassword(@RequestBody HashMap<String, String> args) {
-        try (
-                Connection dbcon = ConnectionController.getConnection();
-                PreparedStatement passwordUpdateStatement = dbcon.prepareStatement(
-                        "UPDATE users SET password = ? WHERE user_id = ?"
-                )
-        ) {
-            System.out.println(Arrays.toString(args.keySet().toArray(new String[0])));
-            String password = args.get("password");
-            String newPassword = args.get("newPassword");
-            long userId = Long.parseLong(args.get("userId"));
-
-            if (!PasswordUtils.isValidPassword(password) || !PasswordUtils.isValidPassword(newPassword)) {
-                return new ResponseEntity<>("Either password or newPassword is invalid", HttpStatus.BAD_REQUEST);
-            }
-
-            if (!UserController.userExists(userId)) {
-                return new ResponseEntity<>("Invalid User ID", HttpStatus.BAD_REQUEST);
-            }
-
-            if (!UserController.passwordMatches(userId, password)) {
-                System.out.println(password);
-                return new ResponseEntity<>("Wrong Password", HttpStatus.BAD_REQUEST);
-            }
-
-            UserController.updatePassword(userId, newPassword);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception exception) {
-            if (exception instanceof NumberFormatException) {
-                return new ResponseEntity<>("Supplied User ID is not a number", HttpStatus.BAD_REQUEST);
-            }
-            exception.printStackTrace();
-        }
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
 }
