ec5cfedecdffa6631e43c22750a552c43cd1795f
flamion <laberschleichi@gmail.com>
2021-04-08 21:13:08 +0200

    Reformat

--- src/main/java/dev/dragoncave/yap/backend/rest/security/tokens/Tokenstore.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/security/tokens/Tokenstore.java
@@ -3,15 +3,15 @@ package dev.dragoncave.yap.backend.rest.security.tokens;
 import java.util.List;
 
 public interface Tokenstore {
-    public List<String> getTokensByUserId(long userId);
+    List<String> getTokensByUserId(long userId);
 
-    public List<String> getTokensByEmail(String emailAddress);
+    List<String> getTokensByEmail(String emailAddress);
 
-    public String createToken(long userId);
+    String createToken(long userId);
 
-    public void invalidateToken(String token);
+    void invalidateToken(String token);
 
-    public void invalidateAllUserTokens(long userId);
+    void invalidateAllUserTokens(long userId);
 
-    public boolean tokenIsValid(String token);
+    boolean tokenIsValid(String token);
 }
