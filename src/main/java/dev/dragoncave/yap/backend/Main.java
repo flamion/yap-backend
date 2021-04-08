32465b43c164af73b82ce315ca419cbcb696c7d3
flamion <laberschleichi@gmail.com>
2021-04-08 21:14:39 +0200

    Removed unused testing code

--- src/main/java/dev/dragoncave/yap/backend/Main.java
+++ src/main/java/dev/dragoncave/yap/backend/Main.java
@@ -1,18 +1,7 @@
 package dev.dragoncave.yap.backend;
 
-import com.google.gson.Gson;
-import dev.dragoncave.yap.backend.databasemanagers.UserController;
-import dev.dragoncave.yap.backend.rest.security.tokens.TokenUtils;
-
-import java.util.HashMap;
-
 public class Main {
-
-    private static final String DATABASE_URL = "jdbc:sqlite:database.db";
-
     public static void main(String[] args) throws Exception {
-        System.out.println(TokenUtils.generateToken());
-        System.out.println(UserController.getUserIdFromEmailAddress("Unixcorn@i-use-arch.com"));
-        System.out.println(UserController.getUserIdFromEmailAddress("asfdsafblasdfhba"));
+
     }
 }
