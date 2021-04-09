f84e1c478d53823faa97dfac33184e975682e9d4
flamion <laberschleichi@gmail.com>
2021-04-09 19:26:44 +0200

    Added user ID and expiry date to the Token class

--- src/main/java/dev/dragoncave/yap/backend/rest/security/tokens/Token.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/security/tokens/Token.java
@@ -2,13 +2,35 @@ package dev.dragoncave.yap.backend.rest.security.tokens;
 
 public class Token {
 
-    private final String token;
+    private String token;
+    private long userId;
+    private long expiryDate;
 
-    public Token(String token) {
-        this.token = token;
+    public Token() {
+
     }
 
     public String getToken() {
         return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(long expiryDate) {
+        this.expiryDate = expiryDate;
     }
 }
