75d06c3280570650e4302426b4bcdc89dcd881aa
flamion <laberschleichi@gmail.com>
2021-04-14 16:58:20 +0200

    Bulk User API Endpoint now uses Token authentication

--- src/main/java/dev/dragoncave/yap/backend/rest/controllers/bulk/BulkUserController.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/controllers/bulk/BulkUserController.java
@@ -1,8 +1,9 @@
 package dev.dragoncave.yap.backend.rest.controllers.bulk;
 
 import dev.dragoncave.yap.backend.databasemanagers.EntryController;
-import dev.dragoncave.yap.backend.databasemanagers.UserController;
 import dev.dragoncave.yap.backend.rest.objects.Entry;
+import dev.dragoncave.yap.backend.rest.security.tokens.DatabaseTokenStore;
+import dev.dragoncave.yap.backend.rest.security.tokens.Tokenstore;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,26 +15,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/bulk/user")
 public class BulkUserController {
+    Tokenstore tokenstore = new DatabaseTokenStore();
 
-    @GetMapping("/{userID}/entries")
-    public ResponseEntity<?> getBulkEntries(@RequestBody List<Long> entryIDs, @PathVariable Long userID) {
+    @GetMapping("/entries")
+    public ResponseEntity<?> getBulkEntries(@RequestHeader(value = "Token") String token, @RequestBody List<Long> entryIDs) {
         try {
-            if (!UserController.userExists(userID)) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            boolean allValid = true;
-            for (var entryID : entryIDs) {
-                if (!EntryController.entryExists(entryID)) {
-                    continue;
-                }
-                if (!EntryController.entryBelongsToUser(userID, entryID)) {
-                    allValid = false;
-                    break;
-                }
+            if (!tokenstore.tokenIsValid(token)) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
             }
 
-            if (!allValid) {
-                return new ResponseEntity<>("Entry ID provided which does not belong to the user", HttpStatus.BAD_REQUEST);
+            long ownerId = tokenstore.getUserIdByToken(token);
+            for (int i = 0; i < entryIDs.size(); i++) {
+                long entryId = entryIDs.get(i);
+                if (!EntryController.entryExists(entryId)) {
+                    entryIDs.remove(i);
+                }
+                if (!EntryController.entryBelongsToUser(ownerId, entryId)) {
+                    entryIDs.remove(i);
+                }
             }
 
             List<Entry> entries = new ArrayList<>();
