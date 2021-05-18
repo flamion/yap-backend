16f7b8adc571df86024613bed644ce8f62d74d03
flamion <laberschleichi@gmail.com>
2021-05-18 17:40:51 +0200

    Fix changePassword Endpoint

--- src/main/java/dev/dragoncave/yap/backend/rest/security/controllers/SecurityController.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/security/controllers/SecurityController.java
@@ -6,13 +6,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.HashMap;
 
-@RestController("/security")
+@RestController()
+@RequestMapping("/security")
 public class SecurityController {
 
 
