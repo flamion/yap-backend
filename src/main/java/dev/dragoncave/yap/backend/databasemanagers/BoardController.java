a54163ad489ec7c7cc3ce158c383f15736f235a2
flamion <laberschleichi@gmail.com>
2021-05-20 13:21:34 +0200

    Added rudimentary Boards.

---
+++ src/main/java/dev/dragoncave/yap/backend/databasemanagers/BoardController.java
@@ -0,0 +1,118 @@
+package dev.dragoncave.yap.backend.databasemanagers;
+
+import dev.dragoncave.yap.backend.databasemanagers.connections.ConnectionController;
+import dev.dragoncave.yap.backend.rest.objects.Board;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("SqlResolve")
+public class BoardController {
+
+	private BoardController() {
+	}
+
+	public static List<Long> getUserBoards(long user_id) throws SQLException {
+		try (
+				Connection dbcon = ConnectionController.getConnection();
+				PreparedStatement getUserBoards = dbcon.prepareStatement(
+						"SELECT board_id FROM member_in_board WHERE user_id = ?"
+				)
+		) {
+			getUserBoards.setLong(1, user_id);
+
+
+			List<Long> boardIDs = new ArrayList<>();
+
+			var resultSet = getUserBoards.executeQuery(); //TODO: decide whether to put in try with resources
+
+			while (resultSet.next()) {
+				boardIDs.add(resultSet.getLong("user_id"));
+			}
+
+			return boardIDs;
+		}
+	}
+
+	public static long createNewBoard(long user_id, String name, long create_date) throws SQLException {
+		try (
+				Connection dbcon = ConnectionController.getConnection();
+				PreparedStatement createNewBoard = dbcon.prepareStatement(
+						"INSERT INTO boards (name, create_date, creator) VALUES (?, ?, ?);",
+						Statement.RETURN_GENERATED_KEYS
+				);
+				PreparedStatement grantBoardAccess = dbcon.prepareStatement(
+						"INSERT INTO member_in_board (user_id, board_id) VALUES (?, ?);" +
+								"INSERT INTO admin_in_board (user_id, board_id) VALUES (?, ?)"
+				)
+		) {
+			createNewBoard.setString(1, name);
+			createNewBoard.setLong(2, create_date);
+			createNewBoard.setLong(3, user_id);
+
+			createNewBoard.execute();
+
+			long newBoardID = -1;
+
+			try (ResultSet generatedKeys = createNewBoard.getGeneratedKeys()) {
+				if (generatedKeys.next()) {
+					newBoardID = generatedKeys.getLong(1);
+				}
+			}
+
+			if (newBoardID == -1) {
+				return newBoardID;
+			}
+
+			grantBoardAccess.setLong(1, user_id);
+			grantBoardAccess.setLong(2, newBoardID);
+			grantBoardAccess.setLong(3, user_id);
+			grantBoardAccess.setLong(4, newBoardID);
+
+			grantBoardAccess.execute();
+
+			return newBoardID;
+		}
+	}
+
+	public static long createNewBoard(long user_id, Board newBoard) throws SQLException {
+		return createNewBoard(user_id, newBoard.getName(), System.currentTimeMillis());
+	}
+
+	public static boolean userHasAccessToBoard(long user_id, long board_id) throws SQLException {
+		try (
+				Connection dbcon = ConnectionController.getConnection();
+				PreparedStatement canAccess = dbcon.prepareStatement(
+						"SELECT * FROM member_in_board WHERE user_id = ? AND board_id = ?"
+				)
+		) {
+			canAccess.setLong(1, user_id);
+			canAccess.setLong(2, board_id);
+
+			return canAccess.executeQuery().next();
+		}
+	}
+
+	public static Board getBoardByID(long board_id) throws SQLException {
+		try (
+				Connection dbcon = ConnectionController.getConnection();
+				PreparedStatement getBoard = dbcon.prepareStatement(
+						"SELECT * FROM boards WHERE board_id = ?"
+				)
+		) {
+			getBoard.setLong(1, board_id);
+
+			try (var boardSet = getBoard.executeQuery()) {
+				boardSet.next();
+				return new Board(
+						board_id,
+						boardSet.getString("name"),
+						boardSet.getLong("create_date"),
+						UserController.getUserByID(boardSet.getLong("creator"))
+				);
+			}
+		}
+	}
+
+}
---
+++ src/main/java/dev/dragoncave/yap/backend/rest/controllers/RestBoardController.java
@@ -0,0 +1,76 @@
+package dev.dragoncave.yap.backend.rest.controllers;
+
+import dev.dragoncave.yap.backend.databasemanagers.BoardController;
+import dev.dragoncave.yap.backend.databasemanagers.UserController;
+import dev.dragoncave.yap.backend.rest.objects.Board;
+import dev.dragoncave.yap.backend.rest.security.tokens.DatabaseTokenStore;
+import dev.dragoncave.yap.backend.rest.security.tokens.Tokenstore;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+
+@RestController
+@RequestMapping("/boards")
+public class RestBoardController {
+	private final Tokenstore tokenstore = new DatabaseTokenStore();
+
+	@GetMapping("/user")
+	public ResponseEntity<?> getUserBoards(@RequestHeader(value = "Token") String token) {
+		try {
+			if (!tokenstore.tokenIsValid(token)) {
+				return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+			}
+
+			long userID = tokenstore.getUserIdByToken(token);
+			return new ResponseEntity<>(BoardController.getUserBoards(userID), HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@GetMapping("/{boardID}")
+	public ResponseEntity<?> getBoard(@RequestHeader(value = "Token") String token, @PathVariable Long boardID) {
+		try {
+			if (!tokenstore.tokenIsValid(token)) {
+				return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+			}
+
+			long userID = tokenstore.getUserIdByToken(token);
+
+			//TODO: Check if board exists to return appropriate Error response, not Forbidden
+			if (!BoardController.userHasAccessToBoard(userID, boardID)) {
+				return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+			}
+
+			return new ResponseEntity<>(BoardController.getBoardByID(boardID), HttpStatus.OK);
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@PostMapping()
+	public ResponseEntity<?> createBoard(@RequestHeader(value = "Token") String token, @RequestBody Board newBoard) {
+		try {
+			if (!tokenstore.tokenIsValid(token)) {
+				return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+			}
+
+			long userID = tokenstore.getUserIdByToken(token);
+			newBoard.setCreator(UserController.getUserByID(userID));
+
+			if (newBoard.boardIsInvalid()) {
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			}
+
+			long newBoardID = BoardController.createNewBoard(userID, newBoard);
+			return new ResponseEntity<>(newBoardID, HttpStatus.CREATED);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
