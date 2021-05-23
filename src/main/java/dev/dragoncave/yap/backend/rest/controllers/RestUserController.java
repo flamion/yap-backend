5aace8a8bccabb0bda2d44da002e2949dff7b42e
flamion <laberschleichi@gmail.com>
2021-05-23 18:43:52 +0200

    Use 307

--- src/main/java/dev/dragoncave/yap/backend/rest/controllers/RestUserController.java
+++ src/main/java/dev/dragoncave/yap/backend/rest/controllers/RestUserController.java
@@ -127,7 +127,7 @@ public class RestUserController {
 			}
 
 			String profilePictureLocation = FileStore.getProfilePictureLocation(userID);
-			return new ResponseEntity<>(profilePictureLocation, HttpStatus.MOVED_TEMPORARILY);
+			return new ResponseEntity<>(profilePictureLocation, HttpStatus.TEMPORARY_REDIRECT);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
